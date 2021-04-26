080fe871b27b25087b1fdbf11649201fd44b7c87
will-ku <william.ku9192@gmail.com>
2021-04-26 12:00:12 -0400

    Completed W7D1 hw

--- w7d1/rails_auth_hw/app/models/user.rb
+++ w7d1/rails_auth_hw/app/models/user.rb
@@ -1,5 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
-    validates :username, :session_token, presence: true
-    validates :password_digest, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: {message: 'Password can\'t be blank'}
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+    before_validation :ensure_session_token
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return user if user != nil && BCrypt::Password.new(user.pass)
+    end
 
 end
