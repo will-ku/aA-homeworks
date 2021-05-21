9aee9c9110a95c1c4d732eab51106e384119aa93
will-ku <william.ku9192@gmail.com>
2021-05-21 11:09:12 -0400

    w10d5 homework

--- w10d2/calculator/calculator.jsx
+++ w10d2/calculator/calculator.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 class Calculator extends React.Component{
   constructor(props){
     super(props);
-    //your code here
+    this.state = { num1: "", num2: "", result: 0}
+
   }
 
   //your code here
@@ -11,8 +12,9 @@ class Calculator extends React.Component{
   render(){
     return (
       <div>
-        <h1>Hello World</h1>
-        //your code will replace this
+        <h1>{this.state.result}</h1>
+        <input onChange={this.setNum1} value={num1}/>
+        <input onChange={this.setNum2} value={num2}/>
       </div>
     );
   }
