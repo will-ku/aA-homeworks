30de8639620cfc85c90f6ed5f1d1a51ad1db5cca
will-ku <william.ku9192@gmail.com>
2021-05-26 15:12:29 -0400

    completed phase 4

---
+++ w11d3/components/rainbow.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
+
+import Red from './red';
+import Green from './green';
+import Blue from './blue';
+import Violet from './violet';
+
+class Rainbow extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Rainbow Router!</h1>
+        {/* Your links should go here */}
+
+        <div id="rainbow">
+          <Route path="/red" component={Red} />
+
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Rainbow;
