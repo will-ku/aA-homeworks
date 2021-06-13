1c5efb08550198499d884910d1a54a75334daaa8
will-ku <william.ku9192@gmail.com>
2021-06-13 16:11:29 -0400

    Working on validators

--- mern-twitter/app.js
+++ mern-twitter/app.js
@@ -6,16 +6,25 @@ const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/users");
+const User = require("./models/User");
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("merp"));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: "jim",
+    email: "jim@jim.jim",
+    password: "abc123",
+  });
+  user.save();
+  res.send("herro world");
+});
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
---
+++ untitled folder/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dev:DuoR8ma2WtPNpAck@cluster0.dxxbg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  //Make sure this is your own unique string
+};
