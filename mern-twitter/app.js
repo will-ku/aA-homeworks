f9bc296f8a7acd226fc5a86f3a5fc857479b2ab1
will-ku <william.ku9192@gmail.com>
2021-06-14 02:09:03 -0400

    Completed hw

--- mern-twitter/app.js
+++ mern-twitter/app.js
@@ -9,6 +9,8 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const User = require("./models/User");
 
+const path = require("path");
+
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully."))
@@ -35,3 +37,10 @@ app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
